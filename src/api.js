b18be0c227f8adc2f458120781824998979a1452
Rafael <rafaelmoura7033@gmail.com>
2024-11-23 14:26:29 -0300

    Atualizando url da api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://api-mediotec-v2.onrender.com/",
+  baseURL: "https://api-mediotec-v2-teste.onrender.com/",
 });
 
 export default api;
